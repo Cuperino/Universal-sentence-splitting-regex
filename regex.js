9e2a73e94a61bd8e7869a5a26ddef5678cb60ea0
Javier O. Cordero Pérez <javier@imaginary.tech>
2018-12-25 21:24:55 -0400

    Update regex.js

--- regex.js
+++ regex.js
@@ -1,4 +1,5 @@
 // Don't pick titles such as Dr. and Mrs.,
 // Don't pick accronyms,
 // Pick sentence's endings.
-(?![A-Z]..?\.)(?![A-Z]?\.)(\b\S+[.?!]["']?)\s
+(?![A-Z]..?\.)(?![A-Z]?\.)(\b\S+[.?!;؟。」]["'』]?)\s
+ 
